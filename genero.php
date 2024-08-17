6da89c133c7de984c1946ecff86035880da83bea
andresVanegas56 <166045730+andresVanegas56@users.noreply.github.com>
2024-08-17 15:07:04 -0500

    Add files via upload

---
+++ genero.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>   
+<link href="./estilos/estilos.css" rel="stylesheet" crossorigin="anonymous">
+</head>
+<body class="cuerpo">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="../index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./genero.php">genero literario </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div class="container">
+    <h1> Adicionar Genero </h1>
+    <form method="POST" action="../bd/adicionarGen.php">
+        <div class="row">
+        <div class="col-2">    
+        <label for="tiquete">Genero</label>
+        </div>
+        <div class="col-4">
+        <input type="text" class="form-control" placeholder="nombre genero" name="genero" required="true"/>
+        </div>
+        </div>
+        <input type="submit" class="btn btn-primary" value="Guardar"></input>
+    </form>
+    <!--Formulario para grabar -->
+</div>    
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./estilos/estilos.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body class="cuerpo">
+    <h1> Listas</h1>
+    <form method="POST" action="./php/mostrar.php">
+        <label for="tiquete">mostrar la lista</label>
+        <input type="submit" value="mostrar"></input>
+        <select name="accion" id="accion">
+            <button> inicia sesion</button>
+
+            
+            <option value="mostrar">mostrar</option>
+            <option value="guardar">guardar</option>
+        </select>
+    </form>
+    <!--Formulario para grabar -- >
+    
+<boby>
+
+</html>
